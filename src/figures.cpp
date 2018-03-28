2af3f2b8cf088d35caf8a9f6bc815318c63a0e18
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-03-28 21:35:58 +1000

    Refactor

--- src/figures.cpp
+++ src/figures.cpp
@@ -28,15 +28,16 @@ double Circle::length()
 double Polyline::length() {
     double sum = 0;
     for (ulong i = 1; i < points_.size(); i++) {
-
         sum += points_[i - 1].distance(points_[i]);
     }
     return sum;
 }
+
 //ASK: any better option?
 Polyline::Polyline(std::vector<std::pair<double, double>> points) {
     points_.reserve(points.size());
-    for (ulong i = 0; i < points.size(); i++) {
-        points_.push_back(Point(points[i].first, points[i].second));
+
+    for (auto &point : points) {
+        points_.emplace_back(point.first, point.second);
     }
 }
